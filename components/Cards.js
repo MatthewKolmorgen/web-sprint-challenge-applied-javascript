b1eb0666a28868ea3a9520814ece9324a1fe71cd
Matthew <mattk.development@gmail.com>
2020-08-07 12:58:01 -0500

    Images not showing up

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,63 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        const articleData = response.data.articles; 
+        Object.values(articleData).forEach((articles) => {
+            articles.forEach((article) => {
+                cardContainer.appendChild(articleMaker(article));
+            })
+        })
+
+    })
+    .catch(error => {
+        console.log('there' + error); 
+}); 
+
+const cardContainer = document.querySelector('.cards-container'); 
+
+
+function articleMaker(object){
+    
+    //Creating elements
+
+    const card = document.createElement('div'); 
+    const headline = document.createElement('div');
+    const author = document.createElement('div'); 
+    const authorDiv = document.createElement('div');
+    const image = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    //Adding Classes
+
+    card.classList.add('card'); 
+    headline.classList.add('headline'); 
+    author.classList.add('author');
+    authorDiv.classList.add('img-container');
+
+    image.setAttribute("src", object.authorPhoto);
+    
+    //Text Content
+
+    headline.textContent = object.headline;
+    authorName.textContent = object.authorName;
+
+    // Setting up page
+
+    card.appendChild(headline); 
+    card.appendChild(author); 
+    // image.src = articleObj.authorPhoto;
+    authorDiv.appendChild(image);
+    author.appendChild(authorDiv);  
+    authorDiv.appendChild(authorName); 
+
+    // event listener 
+    card.addEventListener('click', () => {
+        console.log(headline.textContent); 
+    });
+
+    // return
+    return card; 
+} 
--- components/Tabs.js
+++ components/Tabs.js
@@ -10,18 +10,21 @@
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element 
 
-axios.get('https://lambda-times-api.herokuapp.com/topics')
-    .than(working => {
-        console.log(working);
-        working.data.topics.forEach(info => {
-            const createTopic = document.createElement('div');
-            createTopic.classList('newTab');
-            createTopic.textContent = info;
-            document.querySelector('.title').appendChild(createTopic)
-
-        })
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        response.data.topics.forEach(item => {
+            tabHolder.appendChild(tabMaker(item));
+        }) 
     })
-    .catch(notWorking => {
-	    console.log(notWorking);
-});
+    .catch(error => {
+        console.log(error); 
+    }); 
 
+function tabMaker(topic) {
+    const tab = document.createElement('div'); 
+    tab.classList.add('tab'); 
+    tab.textContent = topic; 
+    return tab;
+} 
+
+const tabHolder = document.querySelector('div.topics'); 
